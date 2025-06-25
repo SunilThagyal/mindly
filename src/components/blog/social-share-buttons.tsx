7188a22660abb31c9952041656a3dae2f576a3b1
Sunil office <suniloffice9311@gmail.com>
2025-06-25 09:37:34 +0000

    <FontAwesomeIcon icon="fa-brands fa-whatsapp" /> add this

--- src/components/blog/social-share-buttons.tsx
+++ src/components/blog/social-share-buttons.tsx
@@ -5,13 +5,8 @@ import { Button } from "@/components/ui/button";
 import { Facebook, Linkedin, Mail, Link as LinkIcon, MoreHorizontal } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { useState, useEffect } from "react";
-
-// A simple inline SVG component for the WhatsApp icon
-const WhatsAppIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg role="img" viewBox="0 0 24 24" fill="currentColor" {...props}>
-        <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-1.04-.01-1.04 0-.523.119-.523.579 0 .429.082.851.082.851l.001.001.001.001c.001.001.046.125.092.231.28.666 1.608 2.857 3.998 4.718 2.388 1.858 2.981 2.25 3.612 2.448.63.198 1.13.165 1.54.105.441-.061 1.341-.557 1.53-1.074.19-.517.19-1 .12-1.124-.07-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.204-1.634a11.815 11.815 0 005.785 1.493h.004c6.554 0 11.88-5.335 11.883-11.893a11.821 11.821 0 00-3.48-8.413Z"/>
-    </svg>
-);
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 interface SocialShareButtonsProps {
   blogUrl: string;
@@ -65,7 +60,7 @@ export default function SocialShareButtons({ blogUrl, blogTitle }: SocialShareBu
     },
     {
       name: "WhatsApp",
-      icon: <WhatsAppIcon className="h-5 w-5" />,
+      icon: <FontAwesomeIcon icon={faWhatsapp} className="h-5 w-5" />,
       url: `https://api.whatsapp.com/send?text=${encodeURIComponent(blogTitle + " " + fullUrl)}`,
     },
     {
