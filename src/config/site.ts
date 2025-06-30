971d0813353c8c1816e6174d3aebca186efa9a50
Sunil office <suniloffice9311@gmail.com>
2025-06-30 17:52:41 +0000

    fixes

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 
 export const siteConfig = {
-  name: "Mindly",
-  url: "https://mindly.devifyo.com", // TODO: Replace with your actual domain
+  name: "Mindily",
+  url: "https://www.mindily.online/", // TODO: Replace with your actual domain
   description: "Shine your ideas on a decentralized blogging platform where you control your content and earn rewards. Connect with a global community and start creating today!",
-  contactEmail: "Mindly@devifyo.com",
+  contactEmail: "officalmindily@gmail.com",
 };
